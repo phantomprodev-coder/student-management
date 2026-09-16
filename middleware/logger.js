66257859bb166a9370f190ca1073120fdead662a
phantomprodev-coder <phantomprodev@gmail.com>
2026-09-16 11:27:27 +0530

    Add files via upload

---
+++ middleware/logger.js
@@ -0,0 +1,9 @@
+const logger = (req, res, next) => {
+    const time = new Date().toLocaleString();
+
+    console.log(`[${time}] ${req.method} ${req.originalUrl}`);
+
+    next();
+};
+
+module.exports = logger;
